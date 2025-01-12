df9059630d034ad9cf274729ba141a06549101af
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-01-12 10:42:41 +0300

    updating the javascript file and Add active link styling to update colors of the pages

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
-
-hamburgerMenu.addEventListener('click', () => {
+hamburgerMenu.addEventListener('click', (e) => {
   navLinks.classList.toggle('show');
+  e.stopPropagation(); 
+});
+
+document.addEventListener('click', (e) => {
+  if (!navLinks.contains(e.target) && !hamburgerMenu.contains(e.target)) {
+    navLinks.classList.remove('show');
+  }
 });
 
 const button = document.querySelector('.say-hi');
